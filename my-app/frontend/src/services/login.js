8069bed7f23b3633d910ffc842dc3098f756623c
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2022-02-08 12:22:09 +0200

    latest changes

---
+++ my-app/frontend/src/services/login.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+const baseUrl = `${process.env.REACT_APP_BACKEND_URL}/api/login`
+
+
+const login = async credentials => {
+	const response = await axios.post(baseUrl, credentials)
+	console.log(response.data)
+	return response.data
+}
+
+export default { login }
